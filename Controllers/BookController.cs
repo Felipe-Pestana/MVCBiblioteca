2209a3439275a4d5c73f4810ce27d069a6226d69
Felipe Pestana <pestana@sitic.com.br>
2023-04-13 10:28:24 -0300

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+
+namespace Controllers
+{
+    public class BookController
+    {
+        public void InsertBook(string t, string a)
+        {
+            Book book = new();
+            book.Title = t;
+            book.Author = a;
+            book.Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ MVCAgenda/Program.cs
@@ -0,0 +1,15 @@
+﻿using Controllers;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Estante Virtual");
+
+        Console.WriteLine("Informe o nome do livro:");
+        string t = Console.ReadLine();
+        Console.WriteLine("Informe o nome do autor: ");
+        string a = Console.ReadLine();
+
+        new BookController().InsertBook(t, a);
+    }
+}
